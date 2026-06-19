502a813930ed8489d3ec9e22225ef6931b286776
Anhaj0 <anhaj02003@gmail.com>
2026-06-19 22:24:24 +0530

    refactor: remove auth and login screens per user request

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginScreen from './pages/auth/LoginScreen';
 import DriverHome from './pages/DriverHome';
 import PassengerHome from './pages/PassengerHome';
-import { PassengerAttendance } from './components/PassengerAttendance';
+import { Attendance as PassengerAttendance } from './pages/passenger/Attendance';
 
 export type RootStackParamList = {
-  Login: undefined;
   DriverHome: undefined;
   PassengerHome: undefined;
   AttendanceDashboard: { passengerId: string };
@@ -20,10 +18,9 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-        initialRouteName="Login"
+        initialRouteName="AttendanceDashboard"
         screenOptions={{ headerShown: false }}
       >
-        <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="AttendanceDashboard" component={PassengerAttendance as any} initialParams={{ passengerId: 'mock-user-1' }} />
       </Stack.Navigator>
     </NavigationContainer>
